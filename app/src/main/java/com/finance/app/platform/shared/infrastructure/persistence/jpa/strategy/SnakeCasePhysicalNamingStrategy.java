c3a9851cdd4119b62ae3956257c824436a87ea01
AndreaCZ16 <bettyandreacotrinazavala@gmail.com>
2024-11-14 13:35:54 -0500

    Actualización de configuración de base de datos

--- app/src/main/java/com/finance/app/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
+++ app/src/main/java/com/finance/app/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -1,4 +1,4 @@
-package com.finance.app.platform.shared.infrastructure.persistence.jpa.strategy;
+ package com.finance.app.platform.shared.infrastructure.persistence.jpa.strategy;
 
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
